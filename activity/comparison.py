96f9b6ce7ce01e0d7f47e49f4843d66a8b2fd60b
Hsiangting <stpeng777@gmail.com>
2025-09-30 18:30:14 -0700

    rewrite get_student_with_more_classes

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,6 +1,16 @@
-def get_student_with_more_classes(*students):
-    most_classes_student = students[0]
-    for student in students[1:]:
-        if student.get_num_classes() > most_classes_student.get_num_classes():
-            most_classes_student = student
+# from student import student
+
+# def get_student_with_more_classes(*students):
+#     most_classes_student = students[0]
+#     for student in students[1:]:
+#         if student.get_num_classes() > most_classes_student.get_num_classes():
+#             most_classes_student = student
+#     return most_classes_student
+
+def get_student_with_more_classes(student, other_student):
+    if student.get_num_classes() >= other_student.get_num_classes():
+        most_classes_student = student
+    else:
+        most_classes_student = other_student
+
     return most_classes_student
